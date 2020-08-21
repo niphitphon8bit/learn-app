ecb14f86748a9a4f8aafb127d642b1c82f443ce5
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-21 17:46:19 +0700

    add function lifecycle

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,35 @@ import React, { Component } from 'react';
 class Header extends Component {
 
     constructor(props) {
+        console.log('constructor');
         super(props);
         this.state = { date: new Date() };
     }
 
+    componentDidMount(){
+        this.timerID = setInterval(() => {
+            this.tick()
+        }, 1000);
+        console.log('did mount');
+    }
+
+    componentDidUpdate(){
+        console.log('did update');
+    }
+
+    componentWillUnmount(){
+        console.log('will un mount');
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        // this.state = {date : new Date()};
+        this.setState({ date: new Date() });
+    }
+
     render() {
         return (
-            <div>{this.state.date.toLocaleDateString()}</div>
+            <div>{this.state.date.toLocaleTimeString()}</div>
         )
     }
 }
