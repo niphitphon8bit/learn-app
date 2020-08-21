7c65ce86d91f7515c4ec179e426bb9879fda33f2
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-21 16:55:31 +0700

    import  - Header, Footer, Productitem component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Footer } from './components/Footer';
+import ProductItem from './components/products/ProductItem';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Hello React
         </p>
         <a
           className="App-link"
@@ -18,7 +21,11 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Header />
+      <ProductItem productName="Keyboard keycron" unitPrice="4000" />
+      <ProductItem productName="Mouse" unitPrice="2000" />
+      <Footer company="peem.co.th" email="niphitphon_nt@hotmail.com" />
     </div>
   );
 }
