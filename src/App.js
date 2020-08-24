c78ced9fe0ce0aa93f8a07d79db392bee466ef37
niphitphon8bit <niphitphon_nt@hotmail.com>
2020-08-24 17:46:15 +0700

    hard code product data

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ class App extends Component {
   componentDidMount() {
     this.setState({
       products: [
-        { product_id: 1, product_name: "Panupong chuaychoo", product_pos: "1", thumbnail: "/src/images/product/1.png", price: 10 },
-        { product_id: 2, product_name: "Niphitphon Thanatkulkit", product_pos: "2", thumbnail: "/src/images/product/2.JPG", price: 20 },
-        { product_id: 3, product_name: "Thakerngsak chuaychoo", product_pos: "3", thumbnail: "/src/images/product/3.png", price: 30 },
-        { product_id: 4, product_name: "Flute Natthanan", product_pos: "4", thumbnail: "/src/images/product/4.png", price: 40 },
-        { product_id: 5, product_name: "Nattapong Tirapongprasert", product_pos: "5", thumbnail: "/src/images/product/5.png", price: 55 },
+        { product_id: 1, product_name: "Panupong chuaychoo", product_pos: "1", thumbnail: "/images/product/1.jpg", price: 10, product_nname: "besslez" },
+        { product_id: 2, product_name: "Niphitphon Thanatkulkit", product_pos: "2", thumbnail: "/images/product/2.JPG", price: 20, product_nname: "nipzbois" },
+        { product_id: 3, product_name: "Thakerngsak chuaychoo", product_pos: "3", thumbnail: "/images/product/3.jpg", price: 30, product_nname: "Pete" },
+        { product_id: 4, product_name: "Flute Natthanan", product_pos: "4", thumbnail: "/images/product/4.jpg", price: 40, product_nname: "Tiny" },
+        { product_id: 5, product_name: "Nattapong Tirapongprasert", product_pos: "5", thumbnail: "/images/product/5.jpg", price: 55, product_nname: "JaKillz" },
       ]
     })
-    console.log("didmount")
+    // console.log(this.state)
   }
 
   render() {
